0b25ad25937c40bce75846de74e01a6e785c8e71
Sunil Salat <sunil.salat@reynasolutions.com>
2023-12-06 16:59:02 +0530

    code refactored

--- for-loop/sort-an-array-of-string.js
+++ for-loop/sort-an-array-of-string.js
@@ -1,16 +1,12 @@
 var a = ["rat", "mat", "fat", "at", "greet"];
 
-const observed = { a: 1, f: 5, m: 15, r: 19, g: 6 };
-
 const sortArray = (list) => {
     const len = list.length;
 
     for (let i = 0; i < len; i++) {
         for (let j = i + 1; j < len; j++) {
             if ((list[i], list[j])) {
-                let firstElem = observed[list[i].charAt(0)];
-                let secondElem = observed[list[j].charAt(0)];
-                if (firstElem > secondElem) {
+                if (list[i].charAt(0) > list[j].charAt(0)) {
                     let temp = list[i];
                     list[i] = list[j];
                     list[j] = temp;
